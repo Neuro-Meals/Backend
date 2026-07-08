d713260c05ed63d33609a87ca87956d5b8087d01
AdamMashaka <mashakaadam123@gmail.com>
2026-07-08 13:29:46 +0300

    new

--- app/modules/notifications/models.py
+++ app/modules/notifications/models.py
@@ -48,3 +48,4 @@ class Notification(Base):
     is_read: Mapped[bool] = mapped_column(Boolean, default=False)
 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    
